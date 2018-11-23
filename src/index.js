985c386db3dfdb3eb56bd50117079392c393bba7
bilibiliYu <coder.zy@qq.com>
2018-11-23 17:57:54 +0800

    add decorate

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 import { createStore,applyMiddleware, compose} from 'redux'
 import { Provider } from 'react-redux'
@@ -18,7 +19,9 @@ const store = createStore(reducers,compose(
 
 ReactDOM.render(
   <Provider store={store}>
-      <App />
+    <HashRouter>
+        <App />
+    </HashRouter>
   </Provider>
   , document.getElementById('root'));
 
---
+++ src/page/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import {
+  $POST_SET_STATE,
+  $POST_EVENT_OPEN,
+  $POST_EVENT_CLOSE,
+  $POST_EVENT_TOGGLE,
+} from '../redux/post'
+
+@connect(
+  store=>({
+    post:store.post,
+    comment:store.comment
+  }),
+  {
+    $POST_SET_STATE,
+    $POST_EVENT_OPEN,
+    $POST_EVENT_CLOSE,
+    $POST_EVENT_TOGGLE,
+  }
+)
+
+class App extends Component {
+  componentDidMount(){
+    this.props.$POST_SET_STATE()
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {this.props.comment.title}
+        </div>
+        <div>
+          {this.props.post.title}
+        </div>
+        {
+          this.props.post.open ? 'open' : 'close'
+        }
+        <div>
+          <button onClick={this.props.$POST_EVENT_OPEN}>open</button>
+          <button onClick={this.props.$POST_EVENT_CLOSE}>close</button>
+          <button onClick={this.props.$POST_EVENT_TOGGLE}>toggle</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
